f0f3fd784dad78a8d75c9b377635d12e12809aeb
Kooneo <kooneo@Kooneos-MacBook-Pro.local>
2025-01-30 14:12:29 +0100

    wip

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: zbakour <zbakour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 13:01:03 by zbakour           #+#    #+#             */
-/*   Updated: 2025/01/30 14:09:24 by zbakour          ###   ########.fr       */
+/*   Updated: 2025/01/30 14:12:29 by zbakour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,13 @@ int	main(int argc, char **argv)
 	
 	// Initialize key states
 	int_keystate(&game);
-	base_image = new_img(game->window->width, game->window->hight, game->window);
+	base_image = new_img(game.window->width, game.window->hight, game.window);
 	game.base_img = &base_image;
 	
 	map_render(&game);
-	mlx_hook(game->window->win, 2, 1L << 0, key_press, &game);
-	mlx_hook(game->window->win, 3, 1L << 1, key_release, &game);
-	mlx_loop_hook(game->window->mlx, handle_player_movement, &game);
-	mlx_loop(game->window->mlx);
+	mlx_hook(game.window->win, 2, 1L << 0, key_press, &game);
+	mlx_hook(game.window->win, 3, 1L << 1, key_release, &game);
+	mlx_loop_hook(game.window->mlx, handle_player_movement, &game);
+	mlx_loop(game.window->mlx);
 	return (0);
 }
