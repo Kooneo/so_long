0aeec3c221c1b867a07fd9d17b0226e81e5fcc52
Kooneo <kooneo@Kooneos-MacBook-Pro.local>
2025-01-31 17:14:10 +0100

    wip

--- utils_2.c
+++ utils_2.c
@@ -6,29 +6,29 @@
 /*   By: zbakour <zbakour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 15:12:55 by zbakour           #+#    #+#             */
-/*   Updated: 2025/01/31 16:17:13 by zbakour          ###   ########.fr       */
+/*   Updated: 2025/01/31 17:14:09 by zbakour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
 
-typedef struct s_map_point
-{
-	int			x;
-	int			y;
-    int        start_x;
-    int        enx_x;
-    int        p_x;
-    int        p_y;
-}				t_map_point;
+// typedef struct s_map_point
+// {
+// 	int			x;
+// 	int			y;
+//     int        start_x;
+//     int        enx_x;
+//     int        p_x;
+//     int        p_y;
+// }				t_map_point;
 
-int ft_x_range(t_map_point point)
-{
-    return ((point.x / 64 >= point.start_x && x / 64 <= point.enx_x) && point.y / 64 == point.p_y);
-}
+// int ft_x_range(t_map_point point)
+// {
+//     return ((point.x / 64 >= point.start_x && x / 64 <= point.enx_x) && point.y / 64 == point.p_y);
+// }
 
-int ft_is_point(t_map_point point)
-{
-    return (point.x / 64 == point.p_x && point.y / 64 == point.p_y) ;
-}
+// int ft_is_point(t_map_point point)
+// {
+//     return (point.x / 64 == point.p_x && point.y / 64 == point.p_y) ;
+// }
