296d25dbce376d0fbe0e69b77a6c86cf5da6f41f
Kooneo <kooneo@Kooneos-MacBook-Pro.local>
2025-02-11 16:18:34 +0100

    wip

--- animation.c
+++ animation.c
@@ -6,13 +6,13 @@
 /*   By: zbakour <zbakour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 16:17:49 by zbakour           #+#    #+#             */
-/*   Updated: 2025/02/11 16:17:11 by zbakour          ###   ########.fr       */
+/*   Updated: 2025/02/11 16:18:28 by zbakour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static char **get_frames_up()
+static char	**get_frames_up()
 {
 	static char	*frames_up[] = {
 		"textures/player/p_up/p_up_00.xpm",
@@ -29,7 +29,7 @@ static char **get_frames_up()
 	return frames_up;
 }
 
-static char **get_frames_down()
+static char	**get_frames_down()
 {
 	static char	*frames_down[] = {
 		"textures/player/p_down/p_down_00.xpm",
@@ -46,7 +46,7 @@ static char **get_frames_down()
 	return frames_down;
 }
 
-static char **get_frames_left()
+static char	**get_frames_left()
 {
 	static char	*frames_left[] = {
 		"textures/player/p_left/xpm/p_left_00.xpm",
@@ -63,7 +63,7 @@ static char **get_frames_left()
 	return frames_left;
 }
 
-static char **get_frames_right()
+static char	**get_frames_right()
 {
 	static char	*frames_right[] = {
 		"textures/player/p_right/p_right_00.xpm",
