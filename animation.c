9c0cb7ba8f19727f2ce6673c22214d237193e539
Kooneo <kooneo@Kooneos-MacBook-Pro.local>
2025-02-11 15:58:59 +0100

    wip

--- animation.c
+++ animation.c
@@ -6,118 +6,68 @@
 /*   By: zbakour <zbakour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 16:17:49 by zbakour           #+#    #+#             */
-/*   Updated: 2025/02/11 15:55:53 by zbakour          ###   ########.fr       */
+/*   Updated: 2025/02/11 15:57:05 by zbakour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static t_direction get_direction(char *direction)
+static char	**get_frames(char *direction, int *frame_count)
 {
-	if (ft_strncmp(direction, "up", 2) == 0)
-		return (UP);
-	if (ft_strncmp(direction, "down", 4) == 0)
-		return (DOWN);
-	if (ft_strncmp(direction, "left", 4) == 0)
-		return (LEFT);
-	if (ft_strncmp(direction, "right", 5) == 0)
-		return (RIGHT);
-	return (INVALID);
-}
-
-static char **get_frames_array(t_direction dir)
-{
-	static char *frames[][9] = {
-		{"textures/player/p_up/p_up_00.xpm", "textures/player/p_up/p_up_01.xpm",
-		"textures/player/p_up/p_up_02.xpm", "textures/player/p_up/p_up_03.xpm",
-		"textures/player/p_up/p_up_04.xpm", "textures/player/p_up/p_up_05.xpm",
-		"textures/player/p_up/p_up_06.xpm", "textures/player/p_up/p_up_07.xpm", NULL},
-		{"textures/player/p_down/p_down_00.xpm", "textures/player/p_down/p_down_01.xpm",
-		"textures/player/p_down/p_down_02.xpm", "textures/player/p_down/p_down_03.xpm",
-		"textures/player/p_down/p_down_04.xpm", "textures/player/p_down/p_down_05.xpm",
-		"textures/player/p_down/p_down_06.xpm", "textures/player/p_down/p_down_07.xpm", NULL},
-		{"textures/player/p_left/p_left_00.xpm", "textures/player/p_left/p_left_01.xpm",
-		"textures/player/p_left/p_left_02.xpm", "textures/player/p_left/p_left_03.xpm",
-		"textures/player/p_left/p_left_04.xpm", "textures/player/p_left/p_left_05.xpm",
-		"textures/player/p_left/p_left_06.xpm", "textures/player/p_left/p_left_07.xpm", NULL},
-		{"textures/player/p_right/p_right_00.xpm", "textures/player/p_right/p_right_01.xpm",
-		"textures/player/p_right/p_right_02.xpm", "textures/player/p_right/p_right_03.xpm",
-		"textures/player/p_right/p_right_04.xpm", "textures/player/p_right/p_right_05.xpm",
-		"textures/player/p_right/p_right_06.xpm", "textures/player/p_right/p_right_07.xpm", NULL}
+	static char	*frames_up[] = {
+		"textures/player/p_up/p_up_00.xpm",
+		"textures/player/p_up/p_up_01.xpm",
+		"textures/player/p_up/p_up_02.xpm",
+		"textures/player/p_up/p_up_03.xpm",
+		"textures/player/p_up/p_up_04.xpm",
+		"textures/player/p_up/p_up_05.xpm",
+		"textures/player/p_up/p_up_06.xpm",
+		"textures/player/p_up/p_up_07.xpm",
+		NULL
 	};
-	if (dir == INVALID)
-		return (NULL);
-	return (frames[dir]);
+	static char	*frames_down[] = {
+		"textures/player/p_down/p_down_00.xpm",
+		"textures/player/p_down/p_down_01.xpm",
+		"textures/player/p_down/p_down_02.xpm",
+		"textures/player/p_down/p_down_03.xpm",
+		"textures/player/p_down/p_down_04.xpm",
+		"textures/player/p_down/p_down_05.xpm",
+		"textures/player/p_down/p_down_06.xpm",
+		"textures/player/p_down/p_down_07.xpm",
+		NULL
+	};
+	static char	*frames_left[] = {
+		"textures/player/p_left/xpm/p_left_00.xpm",
+		"textures/player/p_left/xpm/p_left_01.xpm",
+		"textures/player/p_left/xpm/p_left_02.xpm",
+		"textures/player/p_left/xpm/p_left_03.xpm",
+		"textures/player/p_left/xpm/p_left_04.xpm",
+		"textures/player/p_left/xpm/p_left_05.xpm",
+		"textures/player/p_left/xpm/p_left_06.xpm",
+		"textures/player/p_left/xpm/p_left_07.xpm",
+		NULL
+	};
+	static char	*frames_right[] = {
+		"textures/player/p_right/p_right_00.xpm",
+		"textures/player/p_right/p_right_01.xpm",
+		"textures/player/p_right/p_right_02.xpm",
+		"textures/player/p_right/p_right_03.xpm",
+		"textures/player/p_right/p_right_04.xpm",
+		"textures/player/p_right/p_right_05.xpm",
+		"textures/player/p_right/p_right_06.xpm",
+		"textures/player/p_right/p_right_07.xpm",
+		NULL
+	};
+	if (ft_strncmp(direction, "up", 2) == 0)
+		return (*frame_count = 8, frames_up);
+	if (ft_strncmp(direction, "down", 4) == 0)
+		return (*frame_count = 8, frames_down);
+	if (ft_strncmp(direction, "left", 4) == 0)
+		return (*frame_count = 8, frames_left);
+	if (ft_strncmp(direction, "right", 5) == 0)
+		return (*frame_count = 8, frames_right);
+	return (NULL);
 }
-
-static char **get_frames(char *direction, int *frame_count)
-{
-	t_direction dir;
-	char **frames;
-	dir = get_direction(direction);
-	frames = get_frames_array(dir);
-	if (!frames)
-		return (NULL);
-	*frame_count = 8;
-	return (frames);
-}
-
-// static char	**get_frames(char *direction, int *frame_count)
-// {
-// 	static char	*frames_up[] = {
-// 		"textures/player/p_up/p_up_00.xpm",
-// 		"textures/player/p_up/p_up_01.xpm",
-// 		"textures/player/p_up/p_up_02.xpm",
-// 		"textures/player/p_up/p_up_03.xpm",
-// 		"textures/player/p_up/p_up_04.xpm",
-// 		"textures/player/p_up/p_up_05.xpm",
-// 		"textures/player/p_up/p_up_06.xpm",
-// 		"textures/player/p_up/p_up_07.xpm",
-// 		NULL
-// 	};
-// 	static char	*frames_down[] = {
-// 		"textures/player/p_down/p_down_00.xpm",
-// 		"textures/player/p_down/p_down_01.xpm",
-// 		"textures/player/p_down/p_down_02.xpm",
-// 		"textures/player/p_down/p_down_03.xpm",
-// 		"textures/player/p_down/p_down_04.xpm",
-// 		"textures/player/p_down/p_down_05.xpm",
-// 		"textures/player/p_down/p_down_06.xpm",
-// 		"textures/player/p_down/p_down_07.xpm",
-// 		NULL
-// 	};
-// 	static char	*frames_left[] = {
-// 		"textures/player/p_left/xpm/p_left_00.xpm",
-// 		"textures/player/p_left/xpm/p_left_01.xpm",
-// 		"textures/player/p_left/xpm/p_left_02.xpm",
-// 		"textures/player/p_left/xpm/p_left_03.xpm",
-// 		"textures/player/p_left/xpm/p_left_04.xpm",
-// 		"textures/player/p_left/xpm/p_left_05.xpm",
-// 		"textures/player/p_left/xpm/p_left_06.xpm",
-// 		"textures/player/p_left/xpm/p_left_07.xpm",
-// 		NULL
-// 	};
-// 	static char	*frames_right[] = {
-// 		"textures/player/p_right/p_right_00.xpm",
-// 		"textures/player/p_right/p_right_01.xpm",
-// 		"textures/player/p_right/p_right_02.xpm",
-// 		"textures/player/p_right/p_right_03.xpm",
-// 		"textures/player/p_right/p_right_04.xpm",
-// 		"textures/player/p_right/p_right_05.xpm",
-// 		"textures/player/p_right/p_right_06.xpm",
-// 		"textures/player/p_right/p_right_07.xpm",
-// 		NULL
-// 	};
-// 	if (ft_strncmp(direction, "up", 2) == 0)
-// 		return (*frame_count = 8, frames_up);
-// 	if (ft_strncmp(direction, "down", 4) == 0)
-// 		return (*frame_count = 8, frames_down);
-// 	if (ft_strncmp(direction, "left", 4) == 0)
-// 		return (*frame_count = 8, frames_left);
-// 	if (ft_strncmp(direction, "right", 5) == 0)
-// 		return (*frame_count = 8, frames_right);
-// 	return (NULL);
-// }
 
 void	animate_player(t_game *game, t_paramholder p, char *direction)
 {
