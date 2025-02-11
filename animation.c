cbc275feff0800e48afd31a0561a328242acca54
Kooneo <kooneo@Kooneos-MacBook-Pro.local>
2025-02-11 16:25:54 +0100

    wip

--- animation.c
+++ animation.c
@@ -6,79 +6,11 @@
 /*   By: zbakour <zbakour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 16:17:49 by zbakour           #+#    #+#             */
-/*   Updated: 2025/02/11 16:22:41 by zbakour          ###   ########.fr       */
+/*   Updated: 2025/02/11 16:25:39 by zbakour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-static char	**get_frames_up(void)
-{
-	static char	*frames_up[] = {
-		"textures/player/p_up/p_up_00.xpm",
-		"textures/player/p_up/p_up_01.xpm",
-		"textures/player/p_up/p_up_02.xpm",
-		"textures/player/p_up/p_up_03.xpm",
-		"textures/player/p_up/p_up_04.xpm",
-		"textures/player/p_up/p_up_05.xpm",
-		"textures/player/p_up/p_up_06.xpm",
-		"textures/player/p_up/p_up_07.xpm",
-		NULL
-	};
-
-	return (frames_up);
-}
-
-static char	**get_frames_down(void)
-{
-	static char	*frames_down[] = {
-		"textures/player/p_down/p_down_00.xpm",
-		"textures/player/p_down/p_down_01.xpm",
-		"textures/player/p_down/p_down_02.xpm",
-		"textures/player/p_down/p_down_03.xpm",
-		"textures/player/p_down/p_down_04.xpm",
-		"textures/player/p_down/p_down_05.xpm",
-		"textures/player/p_down/p_down_06.xpm",
-		"textures/player/p_down/p_down_07.xpm",
-		NULL
-	};
-
-	return (frames_down);
-}
-
-static char	**get_frames_left(void)
-{
-	static char	*frames_left[] = {
-		"textures/player/p_left/xpm/p_left_00.xpm",
-		"textures/player/p_left/xpm/p_left_01.xpm",
-		"textures/player/p_left/xpm/p_left_02.xpm",
-		"textures/player/p_left/xpm/p_left_03.xpm",
-		"textures/player/p_left/xpm/p_left_04.xpm",
-		"textures/player/p_left/xpm/p_left_05.xpm",
-		"textures/player/p_left/xpm/p_left_06.xpm",
-		"textures/player/p_left/xpm/p_left_07.xpm",
-		NULL
-	};
-
-	return (frames_left);
-}
-
-static char	**get_frames_right(void)
-{
-	static char	*frames_right[] = {
-		"textures/player/p_right/p_right_00.xpm",
-		"textures/player/p_right/p_right_01.xpm",
-		"textures/player/p_right/p_right_02.xpm",
-		"textures/player/p_right/p_right_03.xpm",
-		"textures/player/p_right/p_right_04.xpm",
-		"textures/player/p_right/p_right_05.xpm",
-		"textures/player/p_right/p_right_06.xpm",
-		"textures/player/p_right/p_right_07.xpm",
-		NULL
-	};
-
-	return (frames_right);
-}
 
 static char	**get_frames(char *direction)
 {
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: zbakour <zbakour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 12:40:35 by zbakour           #+#    #+#             */
-/*   Updated: 2025/02/11 15:59:18 by zbakour          ###   ########.fr       */
+/*   Updated: 2025/02/11 16:25:35 by zbakour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -169,5 +169,10 @@ void			handle_collectibles(t_game *g, int new_x, int new_y);
 void			render_others(t_game *g, int c, int x, int y);
 void			render_wall(t_game *game, t_paramholder p);
 void			render_player(t_game *g, int x, int y);
+
+char			**get_frames_up(void);
+char			**get_frames_down(void);
+char			**get_frames_left(void);
+char			**get_frames_right(void);
 
 #endif
