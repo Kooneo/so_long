dc0cad08349c033a451e284940ce25a0497c57de
Kooneo <kooneo@Kooneos-MacBook-Pro.local>
2025-02-13 17:33:27 +0100

    wip

--- handle_map.c
+++ handle_map.c
@@ -6,7 +6,7 @@
 /*   By: zbakour <zbakour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 13:03:57 by zbakour           #+#    #+#             */
-/*   Updated: 2025/02/13 17:32:45 by zbakour          ###   ########.fr       */
+/*   Updated: 2025/02/13 17:33:24 by zbakour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,14 +78,14 @@ void find_enemies(t_game *game)
     game->map->enemy_count = 0;
     for (i = 0; i < game->map->y; i++)
         for (j = 0; j < game->map->x; j++)
-            if (game->map[i][j] == 'T') // 'E' represents an enemy
+            if (game->map->ptr[i][j] == 'T') // 'E' represents an enemy
 				game->map->enemy_count++;
 
     game->enemies = malloc(sizeof(t_enemy) * game->map->enemy_count);
     index = 0;
     for (i = 0; i < game->map->y; i++)
         for (j = 0; j < game->map->x; j++)
-            if (game->map[i][j] == 'E')
+            if (game->map->ptr[i][j] == 'T')
             {
                 game->enemies[index].x = j;
                 game->enemies[index].y = i;
