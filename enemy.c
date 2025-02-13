e0dfc454e4868c3a9ba93a907c1e9923c6b9a223
Kooneo <kooneo@Kooneos-MacBook-Pro.local>
2025-02-13 18:24:21 +0100

    wip

--- enemy.c
+++ enemy.c
@@ -6,7 +6,7 @@
 /*   By: zbakour <zbakour@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 17:13:17 by zbakour           #+#    #+#             */
-/*   Updated: 2025/02/13 18:23:03 by zbakour          ###   ########.fr       */
+/*   Updated: 2025/02/13 18:24:19 by zbakour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@ void move_enemy(t_game *game, t_enemy *enemy)
         // Clear the old position
         game->map->ptr[enemy->y][enemy->x] = '0';
         // Move to the new position
-        render_image(game, "textures/bg_64n.xpm",  enemy->x, enemy->y);
+        render_image(game, "textures/bg_64n.xpm",  enemy->x * TILE_SIZE, enemy->y * TILE_SIZE);
         enemy->x = new_x;
         game->map->ptr[enemy->y][enemy->x] = 'T';
-        render_image(game, "textures/env/enemy.xpm", new_x, enemy->y);
+        render_image(game, "textures/env/enemy.xpm", new_x * TILE_SIZE, enemy->y * TILE_SIZE);
 
     }
     ft_printf("enemy: {x: %d, y: %d, dir: %d}\n", enemy->x * TILE_SIZE, enemy->y * TILE_SIZE, enemy->direction);
